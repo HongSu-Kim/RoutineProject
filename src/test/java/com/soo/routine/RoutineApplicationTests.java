98bdf2690da05cd95563628ea65ecfe14371b1d4
HS <coldghd@gmail.com>
2022-08-31 21:11:45 +0900

    🎉Create Routine Project

---
+++ src/test/java/com/soo/routine/RoutineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soo.routine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
