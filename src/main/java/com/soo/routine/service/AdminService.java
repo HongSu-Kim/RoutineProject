e1548bb3566c40c0eed2224e3c869e405f1ab243
HS <coldghd@gmail.com>
2022-09-04 14:23:01 +0900

    🚑 Fix ModelMapper
    add ModelMapperConfig

--- src/main/java/com/soo/routine/service/AdminService.java
+++ src/main/java/com/soo/routine/service/AdminService.java
@@ -1,8 +1,11 @@
 package com.soo.routine.service;
 
-import com.soo.routine.dao.BoardDAO;
+import com.soo.routine.domain.Board;
 import com.soo.routine.dto.BoardDTO;
+import com.soo.routine.mapper.AdminMapper;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,11 +14,19 @@ import java.util.List;
 @Service
 public class AdminService {
 
-    private final BoardDAO dao;
+    private final AdminMapper adminMapper;
+    private final ModelMapper modelMapper;
 
-    public List<BoardDTO> getList(String categoryName) {
-        return dao.getList(categoryName);
+    public BoardDTO getBoard(int boardNum) {
+        return modelMapper.map(adminMapper.getBoard(boardNum), BoardDTO.class);
     }
+
+    public List<BoardDTO> getBoardList(String categoryName) {
+//        return modelMapper.map(adminMapper.getBoardList(categoryName), List<Board>);
+        return null;
+    }
+
+
 
 
 
