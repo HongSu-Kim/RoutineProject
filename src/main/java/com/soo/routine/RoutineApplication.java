6b5d4d24bced2eab0fd4eac3a402f912a3a1dde3
s-oo <d-aoo@naver.com>
2022-09-25 23:45:02 +0900

    🚑 Fixing Error

--- src/main/java/com/soo/routine/RoutineApplication.java
+++ src/main/java/com/soo/routine/RoutineApplication.java
@@ -1,9 +1,11 @@
 package com.soo.routine;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan({"com.soo.routine.mapper"})
 public class RoutineApplication {
 
 	public static void main(String[] args) {
