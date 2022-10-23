c21b324146c7657d355bd7a619901b8fe91b9963
s-oo <d-aoo@naver.com>
2022-10-23 23:49:57 +0900

    ✨ Error Message For Withdraw Member

--- src/main/java/com/soo/routine/security/CustomFailureHandler.java
+++ src/main/java/com/soo/routine/security/CustomFailureHandler.java
@@ -1,6 +1,7 @@
 package com.soo.routine.security;
 
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
@@ -26,6 +27,8 @@ public class CustomFailureHandler extends SimpleUrlAuthenticationFailureHandler 
             errorMessage = "이메일이 존재하지 않습니다.";
         } else if (exception instanceof BadCredentialsException) {
             errorMessage = "이메일 또는 비밀번호를 정확하게 입력하세요.";
+        } else if (exception instanceof DisabledException) { // 로그인 5번 실패 시
+            errorMessage = "계정이 비활성화되었습니다. 조금 뒤에 다시 로그인하세요.";
         } else {
             errorMessage = "알 수 없는 이유로 인해 로그인을 할 수 없습니다.";
         }
